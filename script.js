aff0512a15fcde4a3b20159a2bed05b0ee821f28
Davidson <davidson.nando@hotmail.com>
2023-11-28 16:48:46 -0300

    :TADDA:

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle("light")
+
+  var imgElement = document.querySelector("#profile img")
+  var currentImage = imgElement.getAttribute("src")
+
+  if (currentImage.includes("assets/Avatar.png")) {
+    imgElement.setAttribute("src", "assets/Avatar-light.png")
+  } else {
+    imgElement.setAttribute("src", "assets/Avatar.png")
+  }
+}
